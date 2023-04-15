0203bed6f22fb8067e6bf54a31525ede230550a7
Andresss <53404779+Andress-s@users.noreply.github.com>
2023-04-15 09:34:45 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,12 +4,67 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+  private:
+    SYM* head;
+    SYM* tail;
+public:
+    TPQueue() : head(nullptr), tail(nullptr) {}
+    ~TPQueue() {
+        while (head)
+            pop();
+    }
+
+    // добавление элемента в очередь с учетом приоритета
+    void push(char ch, int prior) {
+        SYM* temp = new SYM{ ch, prior, nullptr };
+
+        if (!head) {
+            head = temp;
+            tail = temp;
+        }
+        else if (prior > head->prior) {
+            temp->next = head;
+            head = temp;
+        }
+        else {
+            SYM* current = head;
+            while (current->next && prior <= current->next->prior)
+                current = current->next;
+            temp->next = current->next;
+            current->next = temp;
+        }
+    }
+
+    // удаление элемента из очереди
+    void pop() {
+        if (!head)
+            return;
+
+        SYM* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    // проверка очереди на пустоту
+    bool isEmpty() const {
+        return head == nullptr;
+    }
+
+    // вывод очереди на экран
+    friend ostream& operator<<(ostream& os, const TPQueue& q) {
+        SYM* current = q.head;
+        while (current) {
+            os << '[' << current->ch << ':' << current->prior << ']';
+            current = current->next;
+        }
+        return os;
+    }
 };
 
 struct SYM {
-  char ch;
-  int prior;
+    char ch;
+    int prior;
+    SYM* next;
 };
 
 #endif  // INCLUDE_TPQUEUE_H_
